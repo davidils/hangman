46df9f826750b3ed159f0713644923d661e44a77
David Ils <david_ils@web.de>
2022-07-25 20:15:50 +0200

    - cleanup - added modal window at the end of the game - adjusted styling - added win/loss images

--- script/hangman.js
+++ script/hangman.js
@@ -1,32 +1,6 @@
 let word = '';
 let guess = '';
 let numberOfGuesses = 0;
-
-document.querySelector('.guessesNo').textContent = numberOfGuesses;
-
-const buildWord = function () {
-  for (let i = 0; i < word.length; i++) {
-    guess += '_';
-  }
-  console.log(guess);
-  document.querySelector('.guess').textContent = guess;
-};
-
-const fetchRandomWord = function () {
-  fetch('https://random-word-api.herokuapp.com/word?lang=de')
-    .then(response => response.json())
-    .then(content => {
-      word = content[0].toLowerCase();
-      word = word.replace('ä', 'ae');
-      word = word.replace('ö', 'oe');
-      word = word.replace('ü', 'ue');
-      word = word.replace('ß', 'ss');
-      word = word.toUpperCase();
-      buildWord();
-    });
-};
-fetchRandomWord();
-
 const alphabet = [
   'A',
   'B',
@@ -56,6 +30,43 @@ const alphabet = [
   'Z',
 ];
 
+// TODO replace with hangman image transition
+document.querySelector('.guessesNo').textContent = numberOfGuesses;
+
+const popUpWindow = document.querySelector('.popUp');
+const popUpWindowTitle = document.querySelector('.title');
+const popUpWindowText = document.querySelector('.text');
+const overlay = document.querySelector('.overlay');
+const popUpImg = document.querySelector('.popUpImg');
+
+const createWordToGuess = function () {
+  for (let i = 0; i < word.length; i++) {
+    guess += '_';
+  }
+  document.querySelector('.guess').textContent = guess;
+};
+
+const replaceUmlauts = function (str) {
+  str = str.replace('ä', 'ae');
+  str = str.replace('ö', 'oe');
+  str = str.replace('ü', 'ue');
+  str = str.replace('ß', 'ss');
+  return str;
+};
+
+const fetchRandomWord = function () {
+  fetch('https://random-word-api.herokuapp.com/word?lang=de')
+    .then(response => response.json())
+    .then(content => {
+      word = content[0].toLowerCase();
+      word = replaceUmlauts(word);
+      word = word.toUpperCase();
+      createWordToGuess();
+    });
+};
+
+fetchRandomWord();
+
 const guessLetter = function (letter) {
   document.querySelector(`#LETTER--${letter}`).disabled = true;
 
@@ -68,14 +79,21 @@ const guessLetter = function (letter) {
     document.querySelector('.guess').textContent = guess;
 
     if (guess === word) {
-      alert('You guessed the word!');
+      popUpWindow.classList.remove('hidden');
+      overlay.classList.remove('hidden');
+      popUpWindowTitle.textContent = 'Glückwunsch!';
+      popUpWindowText.textContent = 'Du hast das Wort erraten!';
+      popUpImg.src = 'img/won.jpg';
     }
   } else {
     numberOfGuesses++;
     document.querySelector('.guessesNo').textContent = numberOfGuesses;
-    console.log(`There is no ${letter} in this word!`);
     if (numberOfGuesses === 10) {
-      alert(`You did not guess ${word}!`);
+      popUpWindow.classList.remove('hidden');
+      overlay.classList.remove('hidden');
+      popUpWindowTitle.textContent = `Schade!`;
+      popUpWindowText.textContent = `Das gesuchte Wort lautet: ${word}.`;
+      popUpImg.src = 'img/lost.png';
     }
   }
 };
@@ -84,4 +102,18 @@ for (let i = 0; i < alphabet.length; i++) {
   const letter = alphabet[i];
   const buttonElement = `<input type="button" class="btn--letter" id=LETTER--${letter} value=${letter} onclick="guessLetter('${letter}')"></input>`;
   document.querySelector('.letters').innerHTML += buttonElement;
+}
+
+function startNewGame() {
+  word = '';
+  guess = '';
+  document
+    .querySelectorAll('.btn--letter')
+    .forEach(buttonElement => (buttonElement.disabled = false));
+  numberOfGuesses = 0;
+  document.querySelector('.guessesNo').textContent = numberOfGuesses;
+  popUpWindow.classList.add('hidden');
+  overlay.classList.add('hidden');
+
+  fetchRandomWord();
 }
